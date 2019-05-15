f7c7ab1569ad3d28f8bcedd33a067ef93b8ca85a
cruz-hadassa <50665556+cruz-hadassa@users.noreply.github.com>
2019-05-15 08:18:49 -0500

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,7 @@
+# jnkjn
+print("Hello world")
+happy=input("Enter if you are happy.")
+if(happy=="happy"):
+  print("Good you are happy")
+else(happy!= "happy"):
+  print("Oh no. you are not happy")
