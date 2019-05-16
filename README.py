c67a94be135be25fa1c151431480d2eb228e6f02
cruz-hadassa <50665556+cruz-hadassa@users.noreply.github.com>
2019-05-16 07:59:24 -0500

    Revert "Update README.py"

--- README.py
+++ README.py
@@ -3,7 +3,5 @@ print("Hello world")
 happy=input("Enter if you are happy.")
 if(happy=="happy"):
   print("Good you are happy")
-elif(happy!= "happy"):
+else(happy!= "happy"):
   print("Oh no. you are not happy")
-  print("Hello")
-  
