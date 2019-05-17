6698a93bd6c638b80f26d2ce9b0d5af8648de182
cruz-hadassa <cruz.hadassa@csmstudents.org>
2019-05-17 13:02:41 +0000

    ksjcjdsnkj

--- README.py
+++ README.py
@@ -1,9 +1,10 @@
 # jnkjn
-print("Hello world")
-happy=input("Enter if you are happy.")
-if(happy=="happy"):
-  print("Good you are happy")
-else(happy!= "happy"):
-  print("Oh no. you are not happy")
-print("hi people please work")
-print("i hope this still works")
+print ( "Hello world" )
+happy = input ( "Enter if you are happy." )
+if ( happy=="happy" ):
+  print ( "Good you are happy" ) 
+if ( happy != "happy" ):
+  print ( "Oh no. you are not happy" )
+  print ( "hi people please work" ) 
+  print ( "i hope this still works" )
+  fuwygfueigdfish
