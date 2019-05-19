514b9789b8dae406eca7f636fcb7ed240d668f81
cruz-hadassa <cruz.hadassa@csmstudents.org>
2019-05-19 17:28:30 +0000

    please work

--- README.py
+++ README.py
@@ -2,11 +2,14 @@
 print ( "Hello world" )
 happy = input ( "Enter if you are happy." )
 if ( happy=="happy" ):
-  print ( "Good you are happy" )
+    print("Thats great")
+    print ( "Good you are happy" )
 if ( happy != "happy" ):
-  print ( "Oh no. you are not happy" )
-  print ( "hi people please work" )
-  print ( "i hope this still works" )
-  print ("fuwygfueigdfish")
-  print("how do i run this")
-  hello my friends how are you
+    print ( "Oh no. you are not happy" )
+    print ( "hi people please work" )
+    print ( "i hope this still works" )
+    print ("fuwygfueigdfish")
+    print("how do i run this")
+'''
+i really hope all of this works please for the love of god work
+'''
