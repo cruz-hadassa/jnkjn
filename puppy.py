dd63a59574d010c37ffa9582e4c2b18acb0915df
cruz-hadassa <cruz.hadassa@csmstudents.org>
2019-05-20 03:50:24 +0000

    h

--- puppy.py
+++ puppy.py
@@ -4,9 +4,10 @@ print ( "Good you are happy" )
 print ( "Oh no. you are not happy" )
 print ( "hi people please work" )
 print ( "i hope this still works" )
-print ("fuwygfueigdfish") 
+print ("fuwygfueigdfish")
 print("how do i run this")
 '''
 i really hope all of this works please for the love of god work
 maybe it does work... now i just have to run it :\
 '''
+hi
