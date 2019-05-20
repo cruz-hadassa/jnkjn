6626cef3b3bb4fe9e0e9890672c0f3b2f8aa16b5
cruz-hadassa <cruz.hadassa@csmstudents.org>
2019-05-20 01:06:48 +0000

    new

--- puppy.py
+++ puppy.py
@@ -1,14 +1,11 @@
 print ( "Hello world" )
-happy = input ( "Enter if you are happy." )
-if ( happy=="happy" ):
-    print("Thats great")
-    print ( "Good you are happy" )
-if ( happy != "happy" ):
-    print ( "Oh no. you are not happy" )
-    print ( "hi people please work" )
-    print ( "i hope this still works" )
-    print ("fuwygfueigdfish") 
-    print("how do i run this")
+print("Thats great")
+print ( "Good you are happy" )
+print ( "Oh no. you are not happy" )
+print ( "hi people please work" )
+print ( "i hope this still works" )
+print ("fuwygfueigdfish") 
+print("how do i run this")
 '''
 i really hope all of this works please for the love of god work
 maybe it does work... now i just have to run it :\
